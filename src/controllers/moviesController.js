6cb9a34b953e790ba5d1e040e9ee4d013e0674f0
hellenilda <hellen95limaaraujo@gmail.com>
2024-08-26 12:47:13 -0300

    feat: listar de acordo com a pesquisa e ver detalhes dos filmes

--- src/controllers/moviesController.js
+++ src/controllers/moviesController.js
@@ -1,17 +1,30 @@
 const axios = require('axios')
 
-const getMovieByTitle = async (req, res) => {
+const getMoviesByTitle = async (req, res) => {
     const { title } = req.params
     const apiKey = process.env.OMDB_API_KEY
 
     try {
-        const response = await axios.get(`http://www.omdbapi.com/?t=${title}&apikey=${apiKey}`)
+        const response = await axios.get(`http://www.omdbapi.com/?s=${title}&apikey=${apiKey}`)
         res.json(response.data)
     } catch (error) {
         res.status(500).json({ error: 'Failed to fetch movie data' })
     }
 }
 
+const getMovieDetails = async (req, res) => {
+    const { imdbID } = req.params;
+    const apiKey = process.env.OMDB_API_KEY;
+
+    try {
+        const response = await axios.get(`http://www.omdbapi.com/?i=${imdbID}&apikey=${apiKey}`);
+        res.json(response.data);
+    } catch (error) {
+        res.status(500).json({ error: 'Failed to fetch movie details' });
+    }
+};
+
 module.exports = {
-    getMovieByTitle
+    getMoviesByTitle,
+    getMovieDetails
 }
--- src/routes/movies.js
+++ src/routes/movies.js
@@ -6,6 +6,8 @@ router.get('/', (req, res) => {
     res.render('index')
 })
 
-router.get('/api/movies/:title', moviesController.getMovieByTitle);
+router.get('/api/movies/:title', moviesController.getMoviesByTitle);
+
+router.get('/api/movies/details/:imdbID', moviesController.getMovieDetails);
 
 module.exports = router
